0f1ba7f059f3e93cb8ee5d521faacd9eb00d78c1
2014000001FedericoCotto <f.cotto.5cdb@gmail.com>
2026-03-24 09:57:56 -0600

    Add files via upload

---
+++ Act1_Lecc7_Inc1.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //un algoritmo que determine si un estudiante ganó o perdió segun el promedio
+
+        Console.WriteLine("Hello, World!");
+        //variables
+        string materia;
+        int n1, n2, n3, n4, n5;
+        double promedio;
+
+        //enviar info al usuario
+        Console.WriteLine("Digite una materia: ");
+        materia = Console.ReadLine();
+        Console.WriteLine("Ingrese sus notas: ");
+        n1=Convert.ToInt32(Console.ReadLine());
+        n2=Convert.ToInt32(Console.ReadLine());
+        n3=Convert.ToInt32(Console.ReadLine());
+        n4=Convert.ToInt32(Console.ReadLine());
+        n5=Convert.ToInt32(Console.ReadLine());
+
+        //Proceso
+        promedio = (n1 + n2 + n3 + n4 + n5) / 5;
+
+        //Evaluar si gano o perdio
+        if (promedio >= 70)
+            Console.WriteLine("Estudiante reprobado con: "+promedio);
+        else
+            Console.WriteLine("Estudiante reprobado con: " + promedio);
+    }
+}
